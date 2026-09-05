d5529445a25e240743a0413296170b7d1d965bb1
Madhav Manoj <maadhavmanoj21@gmail.com>
2026-09-05 22:02:21 +0530

    Implement attacker generation penalties

---
+++ src/attacker_policy/__init__.py
@@ -0,0 +1,9 @@
+from .attacker_policy import AttackerPolicy
+from .generation_penalties import (
+    compute_generation_penalty,
+)
+
+__all__ = [
+    "AttackerPolicy",
+    "compute_generation_penalty",
+]
